62a0babdb67eb1fedb063cc589a992b3844203bb
Terence Tuhinanshu <terence@tuhinanshu.com>
2017-09-17 16:01:39 -0400

    Layers LIst

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -8,9 +8,18 @@ export default class Main extends Component {
             <div id="main" class="mdl-grid">
                 <div class="mdl-cell mdl-cell--3-col">
                     <LayerCard type="polygon" />
-                    <LayerCard />
-                    <LayerCard />
-                    <LayerCard />
+                    <div class="layers-list">
+                        <LayerCard />
+                        <LayerCard />
+                        <LayerCard />
+                        <LayerCard />
+                        <LayerCard />
+                        <LayerCard />
+                        <LayerCard />
+                        <LayerCard />
+                        <LayerCard />
+                        <LayerCard />
+                    </div>
                 </div>
                 <div class="mdl-cell mdl-cell--9-col">
                 </div>
