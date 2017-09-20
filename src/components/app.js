94952408dd2880ed1870fb1a4de25576a5fc9d42
Terence Tuhinanshu <terence@tuhinanshu.com>
2017-09-19 22:56:58 -0400

    React Router, Redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,40 +1,34 @@
 import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import { Layout } from 'preact-mdl';
+import { createStore } from 'redux';
 
 import Header from './header';
 import Login from '../routes/login';
 import Main from '../routes/main';
 import Profile from '../routes/profile';
-import Redirect from './redirect';
+
+import reducer from '../redux/reducer';
 // import Home from 'async!./home';
 // import Profile from 'async!./profile';
 
 export default class App extends Component {
-    /** Gets fired when the route changes.
-     *  @param {Object} event       "change" event from [preact-router](http://git.io/preact-router)
-     *  @param {string} event.url   The newly routed URL
-     */
-    handleRoute = e => {
-        this.currentUrl = e.url;
-    };
-
     render() {
         return (
-            <div id="app">
-                <Layout fixed-header>
-                    <Header />
-                </Layout>
-                <Layout.Content>
-                    <Router onChange={this.handleRoute}>
-                        <Login path="/login/" />
-                        <Main path="/main/" />
-                        <Profile path="/profile/" user="me" />
-                        <Profile path="/profile/:user" />
-                        <Redirect path="/" to="/login/" />
-                    </Router>
-                </Layout.Content>
-            </div>
+            <Router>
+                <div id="app">
+                    <Layout fixed-header>
+                        <Header />
+                    </Layout>
+                    <Layout.Content>
+                        <Route path="/login/" component={Login} />
+                        <Route path="/main/" component={Main} />
+                        <Route exact path="/" render={() => (
+                            <Redirect to="/login/" />
+                        )} />
+                    </Layout.Content>
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/redux/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+
+};
+
+export default function reducer(state = initialState, { type, payload }) {
+    switch(type) {
+        default:
+            return state;
+    }
+}
