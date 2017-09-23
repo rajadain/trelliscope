180f892a95bae776c1fbda5ced81c451e7a53285
Terence Tuhinanshu <terence@tuhinanshu.com>
2017-09-23 19:54:49 -0400

    Download Shape

--- src/components/cards/_Card.js
+++ src/components/cards/_Card.js
@@ -2,6 +2,20 @@ import { h, Component } from 'preact';
 import { Button, Card, Icon } from 'preact-mdl';
 
 export default class _Card extends Component {
+    downloadShape() {
+        const filename = `${this.props.params.title}.geojson`;
+        const geojson = encodeURIComponent(this.props.params.geojson);
+
+        const a = document.createElement('a');
+        a.setAttribute('href', `data:text/plain;charset=utf-8,${geojson}`);
+        a.setAttribute('download', filename);
+        a.style.display = 'none';
+
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+
     render({ params, onClear, onVisibilityToggle, icon, className }) {
         return (
             <Card shadow={4} class={`card ${className}`}>
@@ -16,7 +30,9 @@ export default class _Card extends Component {
                     <Button onClick={onVisibilityToggle}>
                         <Icon icon={`visibility${ params.hidden ? " off" : ""}`} />
                     </Button>
-                    <Button><Icon icon="file download" /></Button>
+                    <Button onClick={this.downloadShape.bind(this)}>
+                        <Icon icon="file download" />
+                    </Button>
                 </Card.Actions>
                 <Card.Menu>
                     <Button onClick={onClear}><Icon icon="close" /></Button>
