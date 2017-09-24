94f4d2141800b527f5b3564e2d141664a757c27d
Terence Tuhinanshu <terence@tuhinanshu.com>
2017-09-23 23:31:02 -0400

    Cleaner

--- src/components/cards.js
+++
@@ -1,6 +0,0 @@
-import AddLayer from './cards/AddLayer';
-import AddShape from './cards/AddShape';
-import Layer from './cards/Layer';
-import Shape from './cards/Shape';
-
-export { AddLayer, AddShape, Layer, Shape };
---
+++ src/components/cards/index.js
@@ -0,0 +1,6 @@
+import AddLayer from './AddLayer';
+import AddShape from './AddShape';
+import Layer from './Layer';
+import Shape from './Shape';
+
+export { AddLayer, AddShape, Layer, Shape };
