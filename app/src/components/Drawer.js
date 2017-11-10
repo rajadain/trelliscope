a26f471e186cdcc39c9e5f2526c67e6e92bbf4e6
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-11-10 18:06:11 -0500

    Add Text Filter for Layers

--- app/src/components/Drawer.js
+++ app/src/components/Drawer.js
@@ -1,23 +1,45 @@
 import { h, Component } from 'preact';
-import { Layout, Navigation } from 'preact-mdl';
+import { Layout, Navigation, TextField } from 'preact-mdl';
 import { connect } from 'preact-redux';
 
 import LayerEntry from './LayerEntry';
 
 class Drawer extends Component {
-    render({ layerNames: { data }}) {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filterText: "",
+        };
+    }
+
+    onFilter = (e) => {
+        this.setState({ filterText: e.target.value });
+    }
+
+    render({ layerNames: { data }}, { filterText }) {
         const empty = (<Navigation.Link>No Layers Loaded</Navigation.Link>),
               links = data.length === 0 ? empty :
-                      data.map(({ active, title }, index) => (
-                              <LayerEntry key={index}
-                                          index={index}
-                                          title={title}
-                                          active={active}
-                              />));
+                      data.map(({ active, title }, index) => {
+                              if (!filterText || title.indexOf(filterText) > -1) {
+                                  return <LayerEntry
+                                             key={index}
+                                             index={index}
+                                             title={title}
+                                             active={active}
+                                         />;
+                              }
+                          });
 
         return (
             <Layout.Drawer>
                 <Layout.Title>Add Layer</Layout.Title>
+                <Layout.Title>
+                    <TextField
+                        onKeyUp={this.onFilter}
+                        value={filterText}
+                        placeholder="Filter" />
+                </Layout.Title>
                 <Navigation>
                     {links}
                 </Navigation>
