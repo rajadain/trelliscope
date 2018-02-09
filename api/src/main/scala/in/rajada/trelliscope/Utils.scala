7834b7497facd5ac128bdfdbd8d6afec62eb53cd
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-02-09 16:07:29 -0500

    Add FeatureCollection support
    This allows the user to add a GeoJSON that is a FeatureCollection
    in addition to the existing options of MultiPolygon and Polygon.
    
    Fixes #14

--- api/src/main/scala/in/rajada/trelliscope/Utils.scala
+++ api/src/main/scala/in/rajada/trelliscope/Utils.scala
@@ -1,15 +1,29 @@
 package in.rajada.trelliscope
 
-import geotrellis.vector.{MultiPolygon, Polygon}
+import geotrellis.vector.MultiPolygon
 import geotrellis.vector.io._
+import geotrellis.vector.io.json._
 
-import spray.json.JsValue
+import spray.json.{JsString, JsValue}
 
 trait Utils {
-  def toMultiPolygon(json: JsValue): MultiPolygon = GeometryFormat.read(json) match {
-    case mp: MultiPolygon => mp
-    case p: Polygon => MultiPolygon(Seq(p))
-    case _ => ???
+  def toMultiPolygon(json: JsValue): MultiPolygon = {
+    json.asJsObject.fields("type") match {
+      case JsString("MultiPolygon") => MultiPolygonFormat.read(json)
+      case JsString("Polygon") => MultiPolygon(Seq(PolygonFormat.read(json)))
+      case JsString("FeatureCollection") => {
+        val collection = json.toString.parseGeoJson[JsonFeatureCollection]
+        val mps = collection.getAllMultiPolygons.unionGeometries.asMultiPolygon
+        val ps = collection.getAllPolygons.unionGeometries.asMultiPolygon
+
+        (mps, ps) match {
+          case (Some(mp1), Some(mp2)) => (mp1 union mp2).asMultiPolygon.get
+          case (Some(mp1), _) => mp1
+          case (_, Some(mp2)) => mp2
+          case _ => MultiPolygon()
+        }
+      }
+    }
   }
 
   def toBucketPrefix(s3Path: String): (String, String) = {
